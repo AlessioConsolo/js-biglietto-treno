ec23ff48be2a14b0cc5e7e3ffc79a484815731a7
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-25 18:10:48 +0200

    [UPD] variables updated

--- js/script.js
+++ js/script.js
@@ -5,20 +5,24 @@ console.log(age);
 let price = 0.21 * km;
 
 let perc_discount = 0;
+let final_price = 0;
+
+console.log(price);
 
 if (age < 18) {
   perc_discount = 20;
 } else if (age > 65) {
   perc_discount = 40;
 }
-if (age >= 18 && age <= 65) {
-  perc_discount = 0;
-}
 
 console.log(perc_discount);
-console.log(price);
 
-let final_price = (perc_discount * price) / 100;
+if (perc_discount > 0) {
+  final_price = price - (perc_discount * price) / 100;
+} else if (perc_discount <= 0) {
+  final_price = price;
+}
+
 console.log(final_price.toFixed(2));
 
 document.getElementById("price").innerText = final_price.toFixed(2);
