ffea5e900804e9c5f1d0eac85883676bdffb18b9
AlessioConsolo <alessioconsolo3@gmail.com>
2024-06-25 15:50:22 +0200

    [UPD] Added price and perc_discount variables

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 let km = prompt("Quanti Kilometri desideri percorrere?");
 console.log(km);
-let eta = prompt("Quanti anni hai?");
-console.log(eta);
+let age = prompt("Quanti anni hai?");
+console.log(age);
+let price = 0.21 * km;
+
+let perc_discount = 0;
